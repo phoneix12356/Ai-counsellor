1130e92765dc23c677fd327972e280e3a3bb9578
phoneix12356 <95468972+phoneix12356@users.noreply.github.com>
2026-01-31 18:37:37 +0530

    stupid

--- Backend/index.js
+++ Backend/index.js
@@ -12,14 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Add this after the CORS middleware
-app.use((req, res, next) => {
-  res.on('finish', () => {
-    console.log('CORS Headers set for response:');
-    console.log('Access-Control-Allow-Origin:', res.getHeader('Access-Control-Allow-Origin'));
-    console.log('Access-Control-Allow-Credentials:', res.getHeader('Access-Control-Allow-Credentials'));
-  });
-  next();
-});
+
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -41,7 +34,14 @@ app.use(cors({
   credentials: true
 }));
 
-
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    console.log('CORS Headers set for response:');
+    console.log('Access-Control-Allow-Origin:', res.getHeader('Access-Control-Allow-Origin'));
+    console.log('Access-Control-Allow-Credentials:', res.getHeader('Access-Control-Allow-Credentials'));
+  });
+  next();
+});
 
 
 app.use(express.json());
