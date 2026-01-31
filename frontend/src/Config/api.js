9b6a286fc6138a29566d0f1a8cc709e21522a190
phoneix12356 <95468972+phoneix12356@users.noreply.github.com>
2026-01-31 20:04:03 +0530

    caache;

--- frontend/src/Config/api.js
+++ frontend/src/Config/api.js
@@ -3,6 +3,11 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
+  headers: {
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Expires: "0"
+  }
 });
 
 export default api;
